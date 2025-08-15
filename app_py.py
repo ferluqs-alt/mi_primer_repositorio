b87ddf0032ee01daca71ab6d7279887e3f946962
ferluqs-alt <ferluqs@gmail.com>
2025-08-14 22:05:45 -0500

    Update app_py.py

--- app_py.py
+++ app_py.py
@@ -3,13 +3,13 @@ import pandas as pd
 from sklearn.datasets import fetch_california_housing
 
 # Título de la app
-# --- Estimador de precios de viviendas segun sus caracteristicas inmoviliarias-----
-#  st.title("Análisis del California Housing Dataset")
-st.title("--- Estimador de precios de viviendas segun sus caracteristicas inmoviliarias-----")
+# --- Estimador de precios de viviendas 
+#  st.title("DATA SET: CALIFORNIA HOUSING DATASET")
+st.title("--- ESTIMADOR DE PRECIO DE VIVIENDAS -----")
 # Cargar datos (forzando la descarga si no existe en cache)
 data = fetch_california_housing(as_frame=True, download_if_missing=True)
 df = data.frame
-st.subheader("Análisis exploratorio del California Housing Dataset")
+st.subheader("DATA SET: CALIFORNIA HOUSING DATASET")
 # Mostrar estadísticas básicas
 st.subheader("Estadísticas descriptivas para determinar tendencias")
 st.write(df.describe())
